a661deb5d0ac46dd0d3d36bbf418a3197c90f8e8
lengvangz <lengvang1991@icloud.com>
2022-12-07 17:53:23 -0600

    first commit

--- basic calculator/main.cpp
+++ basic calculator/main.cpp
@@ -1,14 +1,50 @@
-//
-//  main.cpp
-//  basic calculator
-//
-//  Created by Leng Vang on 12/7/22.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+/*
+ the program ask user to enter two floating numbers,
+ then one mathematical symbols (+,-,*,/), the program computes
+ the answer and print the results
+ */
+
+//get and return double from user
+double readNumber()
+{
+    double i;
+    std::cout << "Enter a number with decimal: ";
+    std::cin >> i;
+    return i;
+}
+
+//check the mathematical symbol from user
+char getOperator()
+{
+    char i;
+    std::cout << "Enter one of the following: +, -, *, or /: ";
+    std::cin >> i;
+    return i;
+}
+
+void print(double x, char operation, double y)
+{
+    if(operation == '+')
+        std::cout << x << " + " << y << " is " << x + y << '\n';
+    if(operation == '-')
+        std::cout << x << " - " << y << " is " << x - y << '\n';
+    if(operation == '*')
+        std::cout << x << " * " << y << " is " << x * y << '\n';
+    if(operation == '/')
+        std::cout << x << " / " << y << " is " << x / y << '\n';
+    
+}
+
+int main()
+{
+    double x{ readNumber() };
+    double y{ readNumber() };
+    char operation { getOperator() };
+    
+    print(x, operation, y);
+    
     return 0;
+
 }
