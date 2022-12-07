f7dfbf9756c5c69c5ec9ecd74f814f7281075de7
lengvangz <lengvang1991@icloud.com>
2022-12-07 17:51:35 -0600

    Initial Commit

---
+++ basic calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  basic calculator
+//
+//  Created by Leng Vang on 12/7/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
